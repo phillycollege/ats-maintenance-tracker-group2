32138b02872bc4e507951dac5bb69eb60cd82b72
kingclbk <clbk2608@outlook.com>
2026-04-28 14:22:57 +0100

    Job and Shift class done, WindFarm done.

---
+++ ats-maintenance-tracker-group2/ats-maintenance-tracker-group2/Models/Shift.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ats_maintenance_tracker_group2.Models
+{
+    public class Shift
+    {
+        [Key]
+        public string shiftRecordID { get; set; }
+
+        //Navigational Property
+        public string staffID { get; set; }
+        [ForeignKey(nameof(staffID))]
+        public Staff staff { get; set; }
+        public string shiftType { get; set; }
+        public bool mon { get; set; }
+        public bool tue { get; set; }
+        public bool wed { get; set; }
+        public bool thu { get; set; }
+        public bool fri { get; set; }
+        public bool sat { get; set; }
+        public bool sun {  get; set; }
+
+    }
+}
