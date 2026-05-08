8fea9301ce6948b05cd0e8f1c28f273d935608af
jenya1605 <jenya1605@gmail.com>
2026-05-08 14:38:33 +0100

    staff controller made abracadabra

--- ats-maintenance-tracker-group2/ats-maintenance-tracker-group2/Models/ATSDBContext.cs
+++ ats-maintenance-tracker-group2/ats-maintenance-tracker-group2/Models/ATSDBContext.cs
@@ -23,5 +23,7 @@ namespace ats_maintenance_tracker_group2.Models
         {
             return new ATSDBContext();
         }
+
+        public System.Data.Entity.DbSet<ats_maintenance_tracker_group2.Models.Staff> Staffs { get; set; }
     }
 }
